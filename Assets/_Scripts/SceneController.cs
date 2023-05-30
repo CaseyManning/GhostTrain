ff2093907a6d24bbad3c841c6a0f2e1fde2d6890
Casey Manning <kcgrind@gmail.com>
2023-05-30 09:53:04 -0700

    new teddybear car added

--- Assets/_Scripts/SceneController.cs
+++ Assets/_Scripts/SceneController.cs
@@ -15,7 +15,7 @@ public class SceneController : MonoBehaviour
 
     public static bool dontload = false;
 
-    string[] scenes = { "IntroCar", "TrainCar", "Kitchen", "TrainCar_01", "BarCar"};
+    string[] scenes = { "IntroCar", "TeddyCar", "Kitchen", "TrainCar_01", "BarCar"};
 
     // Start is called before the first frame update
     void Start()
